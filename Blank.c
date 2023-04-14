6b4c7299dab44a088991ff70494623cb0f4e04c7
Nihan Khan <nihan.khan@outlook.com>
2023-04-14 21:54:10 +0600

    Create Blank.c

---
+++ Blank.c
@@ -0,0 +1,19 @@
+/*
+ *******************************************
+ *      * AUTHOR : Nihan_Khan              *
+ *      * NICK   :   NullByte              *
+ *      * CREATED: @@DATETIME:dd.MM.yyyy HH\:mm\:ss@@     *
+ *******************************************
+*/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    
+    
+    return 0;   
+}
